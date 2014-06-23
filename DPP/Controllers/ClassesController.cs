7575eb11ba059f5810d17367b367d452e0e4d499
tmgraves <tadashigraves@gmail.com>
2014-06-23 16:09:24 -0700

    add class scaffold

---
+++ DPP/Controllers/ClassesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DPP.Models;
+
+namespace DPP.Controllers
+{
+    public class ClassesController : Controller
+    {
+        private DanceDBEntities db = new DanceDBEntities();
+
+        // GET: Classes
+        public ActionResult Index()
+        {
+            var events = db.Events.Include(c => c.Group);
+            return View(events.OfType<Class>());
+        }
+
+        // GET: Classes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Events.OfType<Class>().Single(c => c.EventID == id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@class);
+        }
+
+        // GET: Classes/Create
+        public ActionResult Create()
+        {
+            ViewBag.GroupID = new SelectList(db.Groups, "GroupID", "GroupName");
+            return View();
+        }
+
+        // POST: Classes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EventID,EventName,Description,StartDate,EndDate,StartTime,EndTime,AllDay,Link,Cost,LocationType,Address,Address2,City,State,Zip,Country,ParentEventID,Frequency,DaysOfWeek,GroupID")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(@class);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GroupID = new SelectList(db.Groups, "GroupID", "GroupName", @class.GroupID);
+            return View(@class);
+        }
+
+        // GET: Classes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Events.OfType<Class>().Single(c => c.EventID == id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GroupID = new SelectList(db.Groups, "GroupID", "GroupName", @class.GroupID);
+            return View(@class);
+        }
+
+        // POST: Classes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EventID,EventName,Description,StartDate,EndDate,StartTime,EndTime,AllDay,Link,Cost,LocationType,Address,Address2,City,State,Zip,Country,ParentEventID,Frequency,DaysOfWeek,GroupID")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@class).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GroupID = new SelectList(db.Groups, "GroupID", "GroupName", @class.GroupID);
+            return View(@class);
+        }
+
+        // GET: Classes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Events.OfType<Class>().Single(c => c.EventID == id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@class);
+        }
+
+        // POST: Classes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Class @class = db.Events.OfType<Class>().Single(c => c.EventID == id);
+            db.Events.Remove(@class);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
