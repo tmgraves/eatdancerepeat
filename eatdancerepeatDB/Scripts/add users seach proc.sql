947701a850cac4086b40807d8a37d68d2091befb
Jesse Johnson <johnson.jesse@live.com>
2014-06-29 16:51:05 -0700

    Revert "Revert "add users search proc""
    This reverts commit 523d594cc38d52d53c77a48bbeed2b47db27128d.

---
+++ eatdancerepeatDB/Scripts/add users seach proc.sql
@@ -0,0 +1,35 @@
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE name = 'usp_Users_Search')
+DROP PROCEDURE usp_Users_Search
+GO
+CREATE PROC dbo.usp_Users_Search
+/*
+	AUTHOR:		Tad Graves, 06/29/2014
+
+	PURPOSE:		Search for a Users record
+
+	APPLICATION:		eatdancerepeat
+
+	CONTACT:		Tad Graves
+*/
+(
+	@UserID			int= NULL,
+	@FacebookLink			nvarchar(200)= NULL,
+	@UrlName			varchar(200)= NULL
+)
+AS
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	SET NOCOUNT ON
+
+	SELECT 	
+			u.[UserID],
+			u.[FirstName],
+			u.[LastName],
+			u.[JoinDate],
+			u.[FacebookLink],
+			u.[UrlName]
+
+	FROM		Users u
+	WHERE		COALESCE(u.[UserID],0)=COALESCE(@UserID,u.[UserID],0)
+	AND			COALESCE(u.[FacebookLink],'') = COALESCE(@FacebookLink,u.[FacebookLink],'')
+	AND			COALESCE(u.[UrlName],'') = COALESCE(@UrlName,u.[UrlName],'')
+GO
