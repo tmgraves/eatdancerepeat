6b23fc1c61e10aa8cf9f6dac88c468ec38cb26f1
tmgraves <tadashigraves@gmail.com>
2014-06-29 19:01:19 -0700

    populate user table

---
+++ eatdancerepeatDB/Scripts/13 - populate Users table with 2 records.sql
@@ -0,0 +1,24 @@
+INSERT INTO [DanceDB].[dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[JoinDate]
+           ,[FacebookLink]
+           ,[UrlName])
+SELECT     'Vince',
+           'McMahon',
+           '3/14/08',
+           'vmcmahon',
+           'VinceMcMahon1234'
+GO
+INSERT INTO [DanceDB].[dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[JoinDate]
+           ,[FacebookLink]
+           ,[UrlName])
+SELECT     'Steve',
+           'Austin',
+           '10/7/11',
+           'saustin',
+           'SteveAustin8852'
+GO
