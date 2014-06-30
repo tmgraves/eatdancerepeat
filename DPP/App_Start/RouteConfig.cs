52c2eb9796f4072c973f6ec3561a6b140e2c6940
Jesse Johnson <johnson.jesse@live.com>
2014-06-29 19:34:14 -0700

    Added profile route

--- DPP/App_Start/RouteConfig.cs
+++ DPP/App_Start/RouteConfig.cs
@@ -14,28 +14,17 @@ namespace DPP
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Profile",
+                url: "profile/{urlNameOrUserId}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
 
-            routes.MapRoute(
-                name: "Users",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
-                name: "Events",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Events", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
-                name: "Places",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Places", action = "Index", id = UrlParameter.Optional }
-            );
         }
     }
 }
