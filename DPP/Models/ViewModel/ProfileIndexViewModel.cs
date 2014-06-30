4a16a729f1f9e658336f0c9cb8c4bbf6f1d81645
Jesse Johnson <johnson.jesse@live.com>
2014-06-29 19:44:52 -0700

    Updated solution

---
+++ DPP/Models/ViewModel/ProfileIndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DPP.Models.ViewModel
+{
+    public class ProfileIndexViewModel
+    {
+        private DanceDBEntities _db = new DanceDBEntities();
+
+        public ActionResult Index(string urlNameOrUserId)
+        {
+           
+            if (String.IsNullOrWhiteSpace(urlNameOrUserId))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return View();
+        }
+    }
+}
