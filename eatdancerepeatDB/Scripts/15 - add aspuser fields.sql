35c7983a2d1816ae852b10ea98d6cf912bbb27c5
Jesse Johnson <johnson.jesse@live.com>
2014-07-02 16:39:28 -0700

    Added sql script #15

---
+++ eatdancerepeatDB/Scripts/15 - add aspuser fields.sql
@@ -0,0 +1,9 @@
+ALTER TABLE [dbo].[AspNetUsers]
+ADD [Email] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEndDateUtc] [datetime] NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL
